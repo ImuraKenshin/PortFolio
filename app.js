d63ae011b138988b7a434da7f4241b5fbc16ffa2
ImuraKenshin <161824604+ImuraKenshin@users.noreply.github.com>
2024-03-25 11:24:59 +0100

    update 25/03 11h30

--- app.js
+++ app.js
@@ -1,52 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
-    grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-      prev: {
-        shadow: true,
-        translate: [0, 0, -400],
-      },
-      next: {
-        translate: ["100%", 0, 0],
-      },
-    },
-});
-
-let photosGauches = document.querySelectorAll(".photosGauches")
-
-photosGauches.forEach(photo => {
-    photo.addEventListener("click", () => {
-        translateGauche(photo)
-    })
-})
-
-function translateGauche(x) {
-    if (x.classList.contains("init")) {
-        x.classList.remove("init")
-        x.classList.add("translateGauche")
-    }else{
-        x.classList.add("init")
-        x.classList.remove("translateGauche")
-    }
-}
-
-let photosDroites = document.querySelectorAll(".photosDroites")
-
-photosDroites.forEach(photo => {
-    photo.addEventListener("click", () => {
-        translateDroite(photo)
-    })
-})
-
-function translateDroite(x) {
-    if (x.classList.contains("init")) {
-        x.classList.remove("init")
-        x.classList.add("translateDroite")
-    }else{
-        x.classList.add("init")
-        x.classList.remove("translateDroite")
-    }
-}
+window.addEventListener("DOMContentLoaded",()=>{})
 
 /*
 ************************************************************************************************
@@ -56,11 +8,24 @@ function translateDroite(x) {
 
 fetch("./contenu.json")
 .then(data=>{
-  return data.json()
+    return data.json()
 })
 .then(donnees=>{
-    console.log(donnees.projets)
     contenu(donnees.projets)
+    var swiper = new Swiper(".mySwiper", {
+        grabCursor: true,
+        effect: "creative",
+        creativeEffect: {
+            prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+            },
+            next: {
+            translate: ["100%", 0, 0],
+            },
+        },
+    });
+    clicImg()
 })
 
 
@@ -92,9 +57,9 @@ fetch("./contenu.json")
             <img src="https://picsum.photos/id/240/200/300" alt="">
         </div>
         <div class="divPhoto divPhotoDroite">
-            <img id="photo1" class="photosGauches initGauche" src="${donnee.photo1}" alt="">
-            <img id="photo2" class="photosGauches initGauche" src="${donnee.photo2}" alt="">
-            <img id="photo3" class="photosGauches initGauche" src="${donnee.photo3}" alt="">
+            <img id="photo1" class="photosDroites initDroite" src="${donnee.photo1}" alt="">
+            <img id="photo2" class="photosDroites initDroite" src="${donnee.photo2}" alt="">
+            <img id="photo3" class="photosDroites initDroite" src="${donnee.photo3}" alt="">
         </div>
     </div>
 </div>
@@ -105,16 +70,93 @@ let slider = document.getElementById("slider")
 
 /**
  * la fonction ajoute le contenu à mon slider
- * @param {chaine de caractère} X
+ * @param {Array} X
  */
 function contenu(X){
-  // pour chaque objet du json
-  X.forEach(donnee=>{
-    console.log(donnee)
-    // si la position dans le tableau est paire
-    
-
-    // je rajoute un slide gauche à slider
-    // sinon je rajoute un slide droite à slider
-  })
+    slider.innerHTML = `<div class="swiper-slide slide1">
+            <div class="swiper-content flex space-between"></div>
+        </div>`
+    // pour chaque objet du json
+    for (let i = 0; i < X.length; i++) {
+        let donnee = X[i]
+        // si la position dans le tableau est paire
+        if(i%2 == 0){
+            // je rajoute un slide gauche à slider
+            slider.innerHTML += `<div class="swiper-slide slide2">
+                <div class="swiper-content flex space-between">
+                    <div class="divPhoto divPhotoGauche">
+                        <img id="photo1" class="photosGauches initGauche" src="${donnee.photo1}" alt="">
+                        <img id="photo2" class="photosGauches initGauche" src="${donnee.photo2}" alt="">
+                        <img id="photo3" class="photosGauches initGauche" src="${donnee.photo3}" alt="">
+                    </div>
+                    <div class="textContent">
+                        <img src="https://picsum.photos/id/240/200/300" alt="">
+                    </div>
+                </div>
+            </div>`
+        }else{
+            // sinon je rajoute un slide droite à slider
+            slider.innerHTML += `<div class="swiper-slide slide3 flex">
+                <div class="swiper-content flex space-between">
+                    <div class="textContent">
+                        <img src="https://picsum.photos/id/240/200/300" alt="">
+                    </div>
+                    <div class="divPhoto divPhotoDroite">
+                        <img id="photo4" class="photosDroites initDroite" src="${donnee.photo1}" alt="">
+                        <img id="photo5" class="photosDroites initDroite" src="${donnee.photo2}" alt="">
+                        <img id="photo6" class="photosDroites initDroite" src="${donnee.photo3}" alt="">
+                    </div>
+                </div>
+            </div>` 
+        }   
+    }
 }
+
+
+/*
+************************************************************************************************
+********************************************PHOTOS**********************************************
+************************************************************************************************
+*/
+
+//fonction
+
+function clicImg(){
+    let photosGauches = document.querySelectorAll(".photosGauches")
+    
+    photosGauches.forEach(photo => {
+        photo.addEventListener("click", () => {
+            console.log("clic")
+            translateGauche(photo)
+        })
+    })
+    
+    function translateGauche(x) {
+        if (x.classList.contains("init")) {
+            x.classList.remove("init")
+            x.classList.add("translateGauche")
+        }else{
+            x.classList.add("init")
+            x.classList.remove("translateGauche")
+        }
+    }
+    
+    let photosDroites = document.querySelectorAll(".photosDroites")
+    
+    photosDroites.forEach(photo => {
+        photo.addEventListener("click", () => {
+            translateDroite(photo)
+        })
+    })
+    
+    function translateDroite(x) {
+        if (x.classList.contains("init")) {
+            x.classList.remove("init")
+            x.classList.add("translateDroite")
+        }else{
+            x.classList.add("init")
+            x.classList.remove("translateDroite")
+        }
+    }
+}
+
